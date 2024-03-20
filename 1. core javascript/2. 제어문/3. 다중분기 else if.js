c7da69d16bb28b702170f4daf4c4d4310b785e8b
hansolkim9 <masicer2@gmail.com>
2024-03-20 11:29:11 +0900

    # 2-4 제어문(다중분기 else if) 학습

---
+++ 1. core javascript/2. 제어문/3. 다중분기 else if.js
@@ -0,0 +1,14 @@
+
+var age = 16;
+
+if (age >= 20) {
+    console.log('성인입니다.');
+} else if (age >= 17) {
+    console.log('고등학생 입니다.');
+} else if (age >= 14) {
+    console.log('중학생 입니다.');
+} else if (age >= 8) {
+    console.log('초등학생 입니다.');
+} else {
+    console.log('아동 입니다.');
+}
