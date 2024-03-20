2ceaa56eab08abca61f8ed93da50f25b047f9b4a
hansolkim9 <masicer2@gmail.com>
2024-03-20 09:24:03 +0900

    # 2-3 연산자(논리, 조건) 학습

---
+++ 1. core javascript/1. 자바스크립트 기초/5. 논리연산자.js
@@ -0,0 +1,28 @@
+var t = true; f = false;
+
+// and연산: 논리합 - &&
+console.log(t && t);    // t
+console.log(t && f);    // f
+console.log(f && t);    // f
+console.log(f && f);    // f
+
+
+console.log('==============');
+
+// or연산: 논리곱 - ||
+console.log(t || t);    // t
+console.log(t || f);    // t
+console.log(f || t);    // t
+console.log(f || f);    // f
+
+console.log('==============');
+
+// not연산: 논리반전 - !
+console.log(!t);    // f
+
+var money = 0;
+if (!money) {
+    console.log('나는 돈이 없어용!');
+} else {
+    console.log('나는 돈이 있어용!');
+}
