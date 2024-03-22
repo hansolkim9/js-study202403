e8e1a806eec505b9fc78b1d023f5c9efceb7ebcd
hansolkim9 <masicer2@gmail.com>
2024-03-22 09:31:22 +0900

    # 2-4. 제어문 > break 학습

---
+++ 1. core javascript/2. 제어문/15. break.js
@@ -0,0 +1,19 @@
+
+// // i 1부터 50까지 출력 하는데 10보다 커지면 멈춰
+// for (var i = 1 ; i <= 50 ; i++) {
+//     if (i > 10) {
+//         break;
+//     }
+//     console.log(i);
+// }
+
+console.log('============');
+
+for (var i = 0; i < 3 ; i++) {
+    for (var j = 0 ; j < 2 ; j++) {
+        if (i === j) {
+            break
+        }
+        console.log(`${i}, ${j}`);
+    }
+}
